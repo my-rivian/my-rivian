b64cabe65bf5c7385049a789e6442c31178b9ac9
Jiji <obdgeek@obdgeek.com>
2023-07-02 19:29:15 -0400

    Initial Commit

---
+++ MyRivian/MyRivianApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRivianApp.swift
+//  MyRivian
+//
+//  Created by Jiji on 7/2/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRivianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyRivianUITests/MyRivianUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyRivianUITestsLaunchTests.swift
+//  MyRivianUITests
+//
+//  Created by Jiji on 7/2/23.
+//
+
+import XCTest
+
+final class MyRivianUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
